4f77acbd5a79689452c50387037b8b6356a30e3d
Munsif <munsifalimisri69@gmail.com>
2021-03-26 14:09:55 +0500

    lecture -> 7 mini project which gets gihub porular users data

--- 7_fetch_api_get_github_userdata_project/src/App.js
+++ 7_fetch_api_get_github_userdata_project/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [ApiData, setApiData] = useState([]);
+  async function callApiData() {
+    const fetchedData = await fetch("https://api.github.com/users");
+    const data = await fetchedData.json();
+    setApiData(data);
+  }
+  useEffect(() => {
+    callApiData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Github Popular Users</h1>
+      <div class="row">
+        {ApiData.map((val) => {
+          console.log(val);
+          return (
+            <>
+              <div class="col-xs-4">
+                <div class="card">
+                  <img class="card-img-top" src={val.avatar_url} alt="" />
+                  <div class="card-body">
+                    <h5 class="card-title">{val.login}</h5>
+                    <p class="card-text">ID : {val.id}</p>
+                    <div class="row">
+                      <a href={val.repos_url} class="btn btn-primary">
+                        Repositorys
+                      </a>
+                      <a href={val.html_url} class="btn btn-primary">
+                        Link To Github
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 }
